43992e9f884b8238322d463c5594e3995be4999d
kal93 <maaz2dt@gmail.com>
2021-07-25 13:54:54 +0530

    lesson 1 & 2 wrap up

--- src/server.js
+++ src/server.js
@@ -12,4 +12,38 @@ app.use(json())
 app.use(urlencoded({ extended: true }))
 app.use(morgan('dev'))
 
-export const start = () => {}
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello' })
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({ message: 'Ok' })
+})
+app.get('/data', (req, res) => {
+  res.send({ message: '/data hello' })
+})
+
+app.post('/data', (req, res) => {
+  res.send(req.body)
+})
+
+app.post('/saveItem', (req, res) => {
+  console.log(`This was your request body...${JSON.stringify(req.body)}`)
+  console.log(`This was your request headers...${JSON.stringify(req.headers)}`)
+  console.log(`This was your request IP address...${JSON.stringify(req.ip)}`)
+  console.log(
+    `This was your request http version...${JSON.stringify(req.httpVersion)}`
+  )
+  console.log(`This was your host name...${JSON.stringify(req.hostname)}`)
+
+  //   console.log(`This is your response...${JSON.stringify(res.app)}`)
+  console.log(req.body, '...')
+  res.send(302)
+})
+
+export const start = () => {
+  app.listen(3000, () => {
+    console.log(`Server running on 3000...`)
+  })
+}
